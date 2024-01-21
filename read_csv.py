df29c147b73387374824af876169f658f6a7433e
GuillermoAlejandro1491 <gmaguina1491@gmail.com>
2024-01-21 09:21:29 -0600

    generating dictionaries

--- read_csv.py
+++ read_csv.py
@@ -5,12 +5,15 @@ def read_csv(path):
     with open(path, 'r') as csv_file:
         reader = csv.reader(csv_file, delimiter=',')
         header = next(reader)
+        data = []
         for row in  reader:
             iterable = list(zip(header, row))
             country_dict = {key: value for key, value in iterable}
-            print(country_dict)            
+            data.append(country_dict)
+        return data    
+                  
 
             
 if __name__ == '__main__':
-    read_csv('./data.csv')
-                    
+    data = read_csv('./data.csv')
+    print(data[2])
